6478973a82be400571c8f1557e37cd5707a3cfe5
Thalys <thalys@live.com>
2018-08-27 20:15:55 -0300

    Algumas merdas tirada.

--- Grand Chase/Common/Communication/ClientSession.cs
+++ Grand Chase/Common/Communication/ClientSession.cs
@@ -4,22 +4,9 @@ namespace Common.Communication
 {
     public abstract class ClientSession
     {
-        public byte[] GC_DES_KEY;
-        public byte[] HMAC_KEY;
-
-        public ClientSession()
-        {
-            GC_DES_KEY = Generate();
-            HMAC_KEY = Generate();
-        }
-
-        private byte[] Generate()
-        {
-            var bytes = new byte[8];
-            var cryptoServiceProvider = new RNGCryptoServiceProvider();
-            cryptoServiceProvider.GetBytes(bytes);
-            return bytes;
-        }
+        public byte[] GC_DES_KEY = { 0xC7, 0xD8, 0xC4, 0xBF, 0xB5, 0xE9, 0xC0, 0xFD };;
+        public byte[] HMAC_KEY = { 0xC0, 0xD3, 0xBD, 0xC3, 0xB7, 0xCE, 0xB8, 0xB8 };;
+        public int HMAC_SIZE = 10;
         
         public abstract void Receive(byte[] buffer, int size);
     }
