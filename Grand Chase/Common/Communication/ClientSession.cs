2b3a571f31c753faf626c2529762704614c9b464
Thalys <thalys@live.com>
2018-08-27 20:14:15 -0300

    Mexendo na criptografia.

--- Grand Chase/Common/Communication/ClientSession.cs
+++ Grand Chase/Common/Communication/ClientSession.cs
@@ -1,9 +1,25 @@
-﻿namespace Common.Communication
+﻿using System.Security.Cryptography;
+
+namespace Common.Communication
 {
     public abstract class ClientSession
     {
-        public byte[] CryptographyKey = new byte[] {};
-        public byte[] AuthenticKey = new byte[] {};
+        public byte[] GC_DES_KEY;
+        public byte[] HMAC_KEY;
+
+        public ClientSession()
+        {
+            GC_DES_KEY = Generate();
+            HMAC_KEY = Generate();
+        }
+
+        private byte[] Generate()
+        {
+            var bytes = new byte[8];
+            var cryptoServiceProvider = new RNGCryptoServiceProvider();
+            cryptoServiceProvider.GetBytes(bytes);
+            return bytes;
+        }
         
         public abstract void Receive(byte[] buffer, int size);
     }
--- Grand Chase/Common/Communication/Packet/PacketWrite.cs
+++ Grand Chase/Common/Communication/Packet/PacketWrite.cs
@@ -1,7 +1,30 @@
-﻿namespace Common.Communication.Packet
+﻿using System;
+using System.Collections.Generic;
+using Common.Utilities.BigEndian;
+
+namespace Common.Communication.Packet
 {
-    public class PacketWrite
+    public class PacketWrite : IDisposable
     {
+        private int _position { get; set; }
+        private List<byte> _bytes = new List<byte>();
+        public BigEndian BigEndian = new BigEndian();
+
+        private byte[] Bytes => _bytes.ToArray();
         
+        private void Include(byte[] data){
+            _bytes.AddRange(data);
+            _position += data.Length;
+        }
+        
+        public void Int(int int32){
+            Include(BigEndian.GetBytes(int32));
+        }
+
+        public void Dispose()
+        {
+            _position = 0;
+            _bytes = null;
+        }
     }
 }
